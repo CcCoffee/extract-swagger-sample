9a738f08d14e60b2ed73caaf9cd7b2cc30f4a795
Kevin K H Zheng <kevin.k.h.zheng@qq.com>
2024-09-12 23:00:11 +0800

    update api.yml

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -4,9 +4,14 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.Paths;
 import io.swagger.v3.oas.models.media.ArraySchema;
+import io.swagger.v3.oas.models.media.BooleanSchema;
 import io.swagger.v3.oas.models.media.Content;
+import io.swagger.v3.oas.models.media.IntegerSchema;
 import io.swagger.v3.oas.models.media.MediaType;
+import io.swagger.v3.oas.models.media.NumberSchema;
+import io.swagger.v3.oas.models.media.ObjectSchema;
 import io.swagger.v3.oas.models.media.Schema;
+import io.swagger.v3.oas.models.media.StringSchema;
 import io.swagger.v3.parser.OpenAPIV3Parser;
 import io.swagger.v3.parser.core.models.SwaggerParseResult;
 
@@ -50,7 +55,7 @@ public class App {
     }
 
     private static Object generateExampleFromSchema(Schema<?> schema) {
-        if (schema.getType().equals("object")) {
+        if (schema.getType() == null) {
             Map<String, Object> example = new HashMap<>();
             if (schema.get$ref() != null) {
                 // 获取引用的Schema
@@ -64,8 +69,22 @@ public class App {
                 });
             }
             return example;
-        } else if (schema.getType().equals("array")) {
-            Schema<?> itemsSchema = ((ArraySchema)schema).getItems();
+        } else if (schema instanceof ObjectSchema) {
+            Map<String, Object> example = new HashMap<>();
+            if (schema.get$ref() != null) {
+                // 获取引用的Schema
+                String ref = schema.get$ref();
+                Schema<?> refSchema = resolveSchemaReference(ref);
+                return generateExampleFromSchema(refSchema);
+            } else {
+                schema.getProperties().forEach((key, value) -> {
+                    Schema<?> propertySchema = (Schema<?>) value;
+                    example.put(key, generateExampleFromSchema(propertySchema));
+                });
+            }
+            return example;
+        } else if (schema instanceof ArraySchema) {
+            Schema<?> itemsSchema = ((ArraySchema) schema).getItems();
             if (itemsSchema.get$ref() != null) {
                 // 获取引用的Schema
                 String ref = itemsSchema.get$ref();
@@ -87,17 +106,20 @@ public class App {
     }
 
     private static Object getDefaultExampleValue(Schema<?> schema) {
-        switch (schema.getType()) {
-            case "string":
-                return schema.getExample() != null ? schema.getExample() : "string";
-            case "integer":
-                return schema.getExample() != null ? schema.getExample() : 0;
-            case "boolean":
-                return schema.getExample() != null ? schema.getExample() : true;
-            case "number":
-                return schema.getExample() != null ? schema.getExample() : 0.0;
-            default:
-                return null;
+        if (schema instanceof ObjectSchema) {
+            return schema.getExample() != null ? schema.getExample() : new HashMap<>();
+        } else if (schema instanceof ArraySchema) {
+            return schema.getExample() != null ? schema.getExample() : new Object[]{};
+        } else if (schema instanceof StringSchema) {
+            return schema.getExample() != null ? schema.getExample() : "string";
+        } else if (schema instanceof IntegerSchema) {
+            return schema.getExample() != null ? schema.getExample() : 0;
+        } else if (schema instanceof BooleanSchema) {
+            return schema.getExample() != null ? schema.getExample() : true;
+        } else if (schema instanceof NumberSchema) {
+            return schema.getExample() != null ? schema.getExample() : 0.0;
+        } else {
+            return null;
         }
     }
 }
